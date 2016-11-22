8813dee853993fc8504649606d94e7d18db2533e
eneldoserrata <eneldoserrata@gmail.com>
2016-11-21 21:43:58 -0400

    Jus commit

--- orm_api/__openerp__.py
+++ orm_api/__openerp__.py
@@ -27,6 +27,8 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'reports/partner_report.xml',
+
     ],
     # only loaded in demonstration mode
     'demo': [
